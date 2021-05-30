ea59372ed8d4bb7d83cd913c18bde66fe051fcec
Mohamad Reza Safari <mrdev1020@gmail.com>
2021-05-30 22:27:46 +0430

    Add files via upload
    Manage .Net subdomain

---
+++ SubdomainRoute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Domain
+{
+    public class SubdomainRoute : Route
+    {
+        public SubdomainRoute(string domain, string url, RouteValueDictionary defaults)
+            : this(domain, url, defaults, new MvcRouteHandler())
+        {
+        }
+
+        public SubdomainRoute(string domain, string url, object defaults)
+            : this(domain, url, new RouteValueDictionary(defaults), new MvcRouteHandler())
+        {
+        }
+
+        public SubdomainRoute(string domain, string url, object defaults, IRouteHandler routeHandler)
+            : this(domain, url, new RouteValueDictionary(defaults), routeHandler)
+        {
+        }
+
+        public SubdomainRoute(string domain, string url, RouteValueDictionary defaults, IRouteHandler routeHandler)
+            : base(url, defaults, routeHandler)
+        {
+            this.Domain = domain;
+        }
+
+        public string Domain { get; set; }
+
+        public override RouteData GetRouteData(HttpContextBase httpContext)
+        {
+            var routeData = base.GetRouteData(httpContext);
+            routeData.Values.Add("client", httpContext.Request.Url.Host.Split('.').First());
+            return routeData;
+        }
+    }
+}
